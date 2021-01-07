ae37eb845886afab76388827cfc5c56fadc77ff7
manohar <manumanohar1527@gmail.com>
2021-01-07 13:21:24 +0530

    integrated Alan-skd-voice button

--- src/App.js
+++ src/App.js
@@ -1,6 +1,21 @@
-import React from 'react'
+import React, {useEffect} from 'react';
+import alanBtn from "@alan-ai/alan-sdk-web";
+
+const alanKey = process.env.REACT_APP_ALAN_SDK_KEY;
+console.log(alanKey)
 
 function App() {
+  useEffect(()=>{
+    alanBtn({
+      key: alanKey,
+      onCommand: ({command})=>{
+        if(command=="test"){
+          alert("This is code is executed!!");
+        }
+      }
+    })
+  },[])
+
   return (
     <div>
       Alan new application
