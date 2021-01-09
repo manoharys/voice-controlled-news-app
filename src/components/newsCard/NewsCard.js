20d60756a9c7639a109c264286be6434313c931f
manohar <manumanohar1527@gmail.com>
2021-01-09 22:10:57 +0530

    Main page search terms

--- src/components/newsCard/NewsCard.js
+++ src/components/newsCard/NewsCard.js
@@ -1,28 +1,51 @@
-import React from 'react'
-import {Card, CardActions, CardActionArea, CardContent, CardMedia, Button, Typography } from '@material-ui/core'
+import React from "react";
+import {
+  Card,
+  CardActions,
+  CardActionArea,
+  CardContent,
+  CardMedia,
+  Button,
+  Typography,
+} from "@material-ui/core";
 import useStyles from "./Style";
 
-const NewsCard = ({article:{description, publishedAt, source, title, url, urlToImage}, i}) => {
+const NewsCard = ({
+  article: { description, publishedAt, source, title, url, urlToImage },
+  i,
+}) => {
   const classes = useStyles();
-    return (
-        <Card>
-            <CardActionArea>
-             <CardMedia className={classes.media} image={urlToImage} />
-             <div>
-                 <Typography variant="body2" color="textSecondary" component="h2">{(new Date(publishedAt)).toDateString()}</Typography>
-                 <Typography variant="body2" color="textSecondary" component="h2">{source.name} </Typography>
-             </div>
-             <Typography gutterBottom variant="h5">{title}</Typography>
-             <CardContent>
-                 <Typography variant="body2" color="textSecondary" component="p">{description}</Typography>
-             </CardContent>
-            </CardActionArea>
-            <CardActions>
-                <Button size="small" color="prime">Learn more</Button>
-            </CardActions>
-            <Typography variant="body2" component="h5" color="textSecondary">{i+1}</Typography>
-        </Card>
-    )
-}
+  return (
+    <Card className={classes.card}>
+      <CardActionArea href={url} target="_blank">
+        <CardMedia className={classes.media} image={urlToImage} />
+        <div className={classes.details}>
+          <Typography variant="body2" color="textSecondary" component="h2">
+            {new Date(publishedAt).toDateString()}
+          </Typography>
+          <Typography variant="body2" color="textSecondary" component="h2">
+            {source.name}{" "}
+          </Typography>
+        </div>
+        <Typography className={classes.title} gutterBottom variant="h5">
+          {title}
+        </Typography>
+        <CardContent>
+          <Typography variant="body2" color="textSecondary" component="p">
+            {description}
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+      <CardActions className={classes.cardActions}>
+        <Button size="small" color="prime">
+          Learn more
+        </Button>
+        <Typography variant="body2" component="h5" color="textSecondary">
+          {i + 1}
+        </Typography>
+      </CardActions>
+    </Card>
+  );
+};
 
-export default NewsCard
+export default NewsCard;
