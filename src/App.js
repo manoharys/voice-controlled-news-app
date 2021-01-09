3c821fd6b0b8402c17933aac3fedee333f7b99da
manohar <manumanohar1527@gmail.com>
2021-01-09 17:28:50 +0530

    retriving articles from newsApi

--- src/App.js
+++ src/App.js
@@ -1,26 +1,22 @@
-import React, {useEffect} from 'react';
+import React, { useEffect } from "react";
 import alanBtn from "@alan-ai/alan-sdk-web";
 
 const alanKey = process.env.REACT_APP_ALAN_SDK_KEY;
-console.log(alanKey)
+console.log(alanKey);
 
 function App() {
-  useEffect(()=>{
+  useEffect(() => {
     alanBtn({
       key: alanKey,
-      onCommand: ({command})=>{
-        if(command=="test"){
-          alert("This is code is executed!!");
+      onCommand: ({ command, articles }) => {
+        if (command === "topheadlines") {
+          console.log(articles);
         }
-      }
-    })
-  },[])
+      },
+    });
+  }, []);
 
-  return (
-    <div>
-      Alan new application
-    </div>
-  )
+  return <div>Alan new application</div>;
 }
 
-export default App
+export default App;
