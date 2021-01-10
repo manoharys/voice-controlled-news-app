13106dbcc4836115c9cb4df2c2fac7355c0b9530
manohar <manumanohar1527@gmail.com>
2021-01-10 11:09:42 +0530

    implemented searching news by terms

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React, { useEffect, useState } from "react";
 import alanBtn from "@alan-ai/alan-sdk-web";
 import NewsCards from "./components/newsCards/NewsCards";
-
+import useStyles from "./styles";
+import logo from "./images/news.png";
 const alanKey = process.env.REACT_APP_ALAN_SDK_KEY;
 
 function App() {
+  const classes = useStyles();
+
   const [newsArticles, setNewsArticles] = useState([]);
   useEffect(() => {
     alanBtn({
@@ -18,7 +21,14 @@ function App() {
     });
   }, []);
 
-  return <div>{<NewsCards articles={newsArticles} />}</div>;
+  return (
+    <div>
+      <div className={classes.logoContainer}>
+        <img src={logo} alt="newReader" className={classes.Logo} />
+      </div>
+      {<NewsCards articles={newsArticles} />}
+    </div>
+  );
 }
 
 export default App;
