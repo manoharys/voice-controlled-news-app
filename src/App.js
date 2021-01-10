7cdf5c0e701ab068cc87b1b2fb8b87daa9456471
manohar <manumanohar1527@gmail.com>
2021-01-10 13:06:01 +0530

    opening articles based on numbers and going back to home page functionality

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import alanBtn from "@alan-ai/alan-sdk-web";
 import NewsCards from "./components/newsCards/NewsCards";
 import useStyles from "./styles";
 import logo from "./images/news.png";
+import wordsToNumbers from "words-to-numbers";
 const alanKey = process.env.REACT_APP_ALAN_SDK_KEY;
 
 function App() {
@@ -10,22 +11,37 @@ function App() {
 
   const [newsArticles, setNewsArticles] = useState([]);
   const [activeArticle, setActiveArticle] = useState(-1);
+  const [isOpen, setIsOpen] = useState(false);
   useEffect(() => {
     alanBtn({
       key: alanKey,
-      onCommand: ({ command, articles }) => {
+      onCommand: ({ command, articles, number }) => {
         if (command === "newHeadlines") {
-          console.log(articles);
           setNewsArticles(articles);
           setActiveArticle(-1);
-        }
-        if (command === "highlight") {
-          setActiveArticle((prev) => prev + 1);
+        } else if (command === "instructions") {
+          setIsOpen(true);
+        } else if (command === "highlight") {
+          setActiveArticle((prevActiveArticle) => prevActiveArticle + 1);
+        } else if (command === "open") {
+          const parsedNumber =
+            number.length > 2
+              ? wordsToNumbers(number, { fuzzy: true })
+              : number;
+          const article = articles[parsedNumber - 1];
+
+          if (parsedNumber > articles.length) {
+            alanBtn().playText("Please try that again...");
+          } else if (article) {
+            window.open(article.url, "_blank");
+            alanBtn().playText("Opening...");
+          } else {
+            alanBtn().playText("Please try that again...");
+          }
         }
       },
     });
   }, []);
-
   return (
     <div>
       <div className={classes.logoContainer}>
